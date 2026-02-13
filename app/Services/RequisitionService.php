f3017bebeae378aae9fc302ffd12b94bcb4bbeb6
Edmond Zuberi <edmond.norman.zuberi@gmail.com>
2026-02-13 20:59:42 +0300

    Create Requisition Service file

---
+++ app/Services/RequisitionService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+class RequisitionService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
