e083a944a5088974804ab9ee49fec3d47989a60e
Edmond Zuberi <edmond.norman.zuberi@gmail.com>
2026-03-12 18:48:13 +0300

    Added default values to fields tracking items already in order or purchase

---
+++ database/migrations/2026_03_06_072303_add_default_value_to_in_purchase_or_order_column_on_requisition_items_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('requisition_items', function (Blueprint $table) {
+            $table->dropColumn('in_purchase_or_order');
+            $table->boolean('in_purchase_or_order')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('requisition_items', function (Blueprint $table) {
+            $table->dropColumn('in_purchase_or_order');
+            $table->boolean('in_purchase_or_order');
+        });
+    }
+};
