74699bb36578eea0720bbfd341e98bf979f44420
Edmond Zuberi <edmond.norman.zuberi@gmail.com>
2026-03-19 10:20:44 +0300

    Added received_quantity column to purchase and order items

---
+++ database/migrations/2026_03_16_024247_add_received_quantity_column_to_supplier_order_items_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('supplier_order_items', function (Blueprint $table) {
+            $table->decimal('received_quantity')->default(0.00)->after('quantity');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('supplier_order_items', function (Blueprint $table) {
+            $table->dropColumn('received_quantity');
+        });
+    }
+};
