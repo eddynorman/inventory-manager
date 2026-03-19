6cd8771972804ed36db021bce3dedf9cb98a36c9
Edmond Zuberi <edmond.norman.zuberi@gmail.com>
2026-03-19 10:25:21 +0300

    Made purchase id nullable in receivings table

---
+++ database/migrations/2026_03_16_092132_make_purchase_id_nullable_in_receivings_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('receivings', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('purchase_id');
+            $table->foreignId('purchase_id')->nullable()->after('id')->constrained('purchases','id')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('receivings', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('purchase_id');
+            $table->foreignId('purchase_id')->after('id')->constrained('purchases','id')->cascadeOnDelete();
+        });
+    }
+};
