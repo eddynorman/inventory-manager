8a39e15a859a3c5e1952cdb727edcc2737395a15
Edmond Zuberi <edmond.norman.zuberi@gmail.com>
2026-03-26 16:55:48 +0300

    Changed purchase to receiving and added transfer to the enum.

--- app/Enums/StockBatchType.php
+++ app/Enums/StockBatchType.php
@@ -6,18 +6,20 @@ enum StockBatchType: string
 {
     case ITEM_SALE = 'item_sale';
     case KIT_SALE = 'kit_sale';
-    case PURCHASE = 'purchase';
+    case RECEIVING = 'receiving';
     case ADJUSTMENT = 'adjustment';
     case ADJUSTMENT_NEGATIVE = 'adjustment_negative';
+    case TRANSFER = 'transfer';
     // Optional: label for UI
     public function label(): string
     {
         return match($this) {
             self::ITEM_SALE => 'Item Sale',
             self::KIT_SALE => 'Kit Sale',
-            self::PURCHASE => 'Purchase',
+            self::RECEIVING => 'Receiving',
             self::ADJUSTMENT => 'Adjustment',
             self::ADJUSTMENT_NEGATIVE => 'Adjustment Negative',
+            self::TRANSFER => 'Transfer',
         };
     }
 }
