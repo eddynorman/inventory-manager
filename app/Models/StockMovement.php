22a43d826d615dbd164938cb00bb5ee67b37f173
Edmond Zuberi <edmond.norman.zuberi@gmail.com>
2026-03-26 16:49:51 +0300

    Implemented relationship methods and listed fillable properties.

--- app/Models/StockMovement.php
+++ app/Models/StockMovement.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class StockMovement extends Model
 {
-    //
+    protected $fillable = ['item_id','from_location','to_location','quantity','type','reference_type','reference_id','created_by'];
+
+    public function item(){
+        return $this->belongsTo(Item::class,'item_id');
+    }
+
+    public function to(){
+        return $this->belongsTo(Location::class,'to_location');
+    }
+
+     public function from(){
+        return $this->belongsTo(Location::class,'from_location');
+    }
 }
