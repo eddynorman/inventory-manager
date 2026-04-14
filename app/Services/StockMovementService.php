52c676c07c536cbbcdd10fe2f845e0734803ef80
Edmond Zuberi <edmond.norman.zuberi@gmail.com>
2026-04-14 21:08:17 +0300

    feat(StockMovement): Fixed naming bugs. - Changed from_location_id to from_location to match database. - Added get to the eloquent builder so as to fetch actual results. - Changed saleId to reference_id so that it may be more general.

--- app/Services/StockMovementService.php
+++ app/Services/StockMovementService.php
@@ -11,30 +11,30 @@ use Illuminate\Support\Facades\DB;
 class StockMovementService
 {
 
-    public function createMovement(int $itemId,int $fromLocation, ?int $toLocation,float $quantity, string $type, StockBatchType $reference_type,int $saleId, int $userId){
+    public function createMovement(int $itemId,int $fromLocation, ?int $toLocation,float $quantity, string $type, StockBatchType $reference_type,int $reference_id, int $userId){
         return StockMovement::create([
             'item_id'          => $itemId,
-            'from_location_id' => $fromLocation,
+            'from_location' => $fromLocation,
             'to_location' => $toLocation,
             'quantity'         => $quantity, //negative for stock reduction
             'type'             => $type,
             'reference_type'   => $reference_type->value,
-            'reference_id'     => $saleId,
+            'reference_id'     => $reference_id,
             'created_by'       => $userId,
         ]);
     }
 
     public function saleItemsSync(array $saleItems, int $saleId, int $userId, StockBatchType $type){
         return DB::transaction(function () use ($saleItems,$saleId, $userId,$type){
-                foreach($saleItems as $item){
-                $movement = StockMovement::where('reference_id',$saleId)->where('reference_type',$type->value)->where('item_id',$item['item_id']);
+            foreach($saleItems as $item){
+                $movement = StockMovement::where('reference_id',$saleId)->where('reference_type',$type->value)->where('item_id',$item['item_id'])->get();
                 if(!$movement->isEmpty() && $movement->quantity != -$item['quantity']){
                     $movement->quantity = -$item['quantity'];
                     $movement->save();
                 }else{
                     StockMovement::create([
                         'item_id'          => $item['item_id'],
-                        'from_location_id' => $item['location_id'],
+                        'from_location' => $item['location_id'],
                         'quantity'         => -$item['quantity'],
                         'type'             => "Item Sale",
                         'reference_type'   => $type->value,
