90dea32228325df28b3cb4464e5bd36b566ac7c8
Edmond Zuberi <edmond.norman.zuberi@gmail.com>
2026-04-15 13:06:15 +0300

    Added adjustment_item_id to fillable properties

--- app/Models/SaleItemBatch.php
+++ app/Models/SaleItemBatch.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class SaleItemBatch extends Model
 {
-    protected $fillable = ['sale_item_id','sale_item_kit_item_id','stock_batch_id','quantity','unit_cost','total','type','type_id'];
+    protected $fillable = ['sale_item_id','sale_item_kit_item_id','adjustment_item_id','stock_batch_id','quantity','unit_cost','total','type','type_id'];
 }
