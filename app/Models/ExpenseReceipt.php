733742e191070fccfb04ff6f0f02da6dde347bdd
Edmond Zuberi <edmond.norman.zuberi@gmail.com>
2026-04-25 21:50:02 +0300

    Created Expense Receipts Model.

---
+++ app/Models/ExpenseReceipt.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ExpenseReceipt extends Model
+{
+    protected $fillable = ['expense_id','receipt_path'];
+
+    public function expense(){
+        return $this->belongsTo(Expense::class,'expense_id');
+    }
+}
