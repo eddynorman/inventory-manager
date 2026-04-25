7e67dcf517bf053e50987c29a592a606e9fa3baf
Edmond Zuberi <edmond.norman.zuberi@gmail.com>
2026-04-25 21:47:01 +0300

    Created Expense Category Model.

---
+++ app/Models/ExpenseCategory.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ExpenseCategory extends Model
+{
+    protected $fillable = ['name','description'];
+
+    public function expenses(){
+        return $this->hasMany(Expense::class,'expense_category_id');
+    }
+}
