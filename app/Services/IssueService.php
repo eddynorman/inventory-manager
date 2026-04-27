ee6117855514146a7c220a24bb1c4fe04ac83940
Edmond Zuberi <edmond.norman.zuberi@gmail.com>
2026-04-27 20:05:45 +0300

    Created Issue service class.

---
+++ app/Services/IssueService.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Issue;
+use App\Models\IssueItem;
+use App\Models\Item;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class IssueService
+{
+    public $locationId;
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function rules(){
+        return [
+            'locationId' => ['required','integer','exists:locations,id'],
+            'destinationId' => ['required','integer','exists:locations,id'],
+            'description' => ['nullable','string'],
+            'issueItems' => ['required','array','min:1'],
+            'issueItems.*' => ['required','array','min:4'],
+            'issueItems.*.item_id' => ['required','integer','exists:items,id'],
+            'issueItems.*.selected_unit_id' => ['required','integer','exists:units,id'],
+            'issueItems.*.stock' => ['required','numeric'],
+            'issueItems.*.quantity' => ['required','numeric'],
+        ];
+    }
+
+    public function search(int $locationId,string $search):array
+    {
+        $this->locationId = $locationId;
+        $items = Item::where('name', 'like', '%' . $search . '%')
+            ->where('is_active',true)
+            ->whereHas('locationItems', function ($q) {
+                $q->where('location_id',$this->locationId);
+            })
+            ->with(['locationItems' => function ($q) {
+                $q->where('location_id', $this->locationId);
+            }])
+            ->with('units')
+            ->get()
+            ->map(function ($item) {
+
+                $stock = $item->locationItems->first()->quantity;
+
+                return [
+                    'item_id' => $item->id,
+                    'name' => $item->name,
+                    'stock' => $stock,
+                    'quantity' => 1,
+                    'units' => $item->units->toArray(),
+                    'selected_unit_id' => $item->units->first()->id,
+                ];
+            })->toArray();
+        return $items;
+    }
+
+    public function getById(int $id):Issue{
+        return Issue::with(['items.item','processedBy','rejectedBy','user','fromLocation','toLocation'])->findOrFail($id);
+    }
+
+    public function save(array $data, ?int $issueId = null){
+       return DB::transaction(function() use ($data,$issueId){
+            $issue = Issue::updateOrCreate(
+                ['id'=>$issueId ?? null],
+                [
+                    'description' => $data['description'],
+                    'from_location_id' => $data['locationId'],
+                    'to_location_id' => $data['destinationId'],
+                    'issue_date' => Carbon::today(),
+                    'status' => 'pending',
+                    'user_id' => Auth::id(),
+                ]
+            );
+
+            foreach($data['issueItems'] as $item){
+                IssueItem::updateOrCreate(['id' => $item['id'] ?? null],
+                    [
+                        'issue_id' => $issue->id,
+                        'item_id' => $item['item_id'],
+                        'quantity' => $item['quantity'],
+                        'unit_id' => $item['selected_unit_id'],
+                    ]
+                );
+            }
+            return $issue;
+        });
+    }
+}
