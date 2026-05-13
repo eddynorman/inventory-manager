1e296cdefaeabd5b4c4980b2d2c526f0c1ce03f0
Edmond Zuberi <edmond.norman.zuberi@gmail.com>
2026-05-13 14:04:06 +0300

    Removed role from the seed as it is no longer used.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,7 +17,6 @@ class UserSeeder extends Seeder
                 'name' => 'Super Admin',
                 'password' => Hash::make('password'),
                 'type' => 'staff',
-                'role' => 'super',
             ]
         );
 
